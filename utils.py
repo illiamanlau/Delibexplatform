de6490c3ed700cbc954c128910a3770c3fe1db4f
Victor <victormartin96@outlook.com>
2024-09-27 11:54:33 +0300

    Move constants to constants.py

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import random
 import re
 import asyncio
 
+from constants import *
+
 def print_json(data, force_print = False):
     if force_print:
         print(json.dumps(data, indent=2))
@@ -32,20 +34,10 @@ def build_message(role, content, name = None):
     else:
         return {'role' : role, 'content' : content, 'name' : name}
 
-SPEED_UP_FACTOR = 1.0
-
 async def bot_sleep(time_s, logger=None):
     if logger:
         logger.info(f'Going to sleep for {time_s/SPEED_UP_FACTOR}s')
     await asyncio.sleep(time_s/SPEED_UP_FACTOR)
-
-S_TO_MS = 10**3
-READ_SPEED = 30 # Characters per second
-MIN_IDLE_READ_TIME_MS = 0
-MAX_IDLE_READ_TIME_MS = 1000
-WRITE_SPEED = 5 # Characters per second
-MIN_IDLE_WRITE_TIME_MS = 2000
-MAX_IDLE_WRITE_TIME_MS = 7000
 
 def get_reading_time(message_len: int):
     return (message_len/READ_SPEED + random.randint(MIN_IDLE_READ_TIME_MS, MAX_IDLE_READ_TIME_MS)/S_TO_MS)/SPEED_UP_FACTOR
